850ee462ff4f0ce2379350c29f91f4e2fbf18648
Rock1ey <2061218098@qq.com>
2024-06-10 17:23:12 +0800

    语音识别测试代码，并加入到按钮中(已注释，方便运行界面)

--- speech recognition.py
+++ speech recognition.py
@@ -1,12 +1,44 @@
 import tkinter as tk
 
+# import threading
+# import speech_recognition as sr
+
+# # 创建识别器对象
+# recognizer = sr.Recognizer()
+# is_listening = False
+
 def start_voice_recognition():
-# 在这里添加语音识别逻辑
-  pass
+    # 在这里添加开始语音识别逻辑
+    # global is_listening # 全局变量，多个函数共享
+    # is_listening = True # 设置为开始录音
+    # threading.Thread(target=listen_in_background).start() # 开启线程
+    pass
 
 def stop_voice_recognition():
-# 在这里添加停止语音识别的逻辑
-  pass
+    # 在这里添加停止语音识别的逻辑
+    # global is_listening
+    # is_listening = False # 设置为停止录音
+    # print("Listening stopped:", is_listening)
+    pass
+
+# def listen_in_background():
+#     # 线程：语音输入过程
+#     global is_listening
+#     with sr.Microphone() as source:
+#         if is_listening:
+#             print("请说话...")
+#             recognizer.adjust_for_ambient_noise(source)
+#             try:
+#                 audio_data = recognizer.listen(source, timeout=10, phrase_time_limit=5)
+#                 text = recognizer.recognize_sphinx(audio_data, language='zh-CN')
+#                 is_listening = True
+#                 print("你说的是: " + text)
+#             except sr.UnknownValueError:
+#                 print("无法识别音频")
+#             except sr.RequestError as e:
+#                 print("无法请求结果；{0}".format(e))
+#             except sr.WaitTimeoutError:
+#                 print("等待超时，没有检测到语音输入")
 
 #创建主窗口
 root = tk.Tk()
